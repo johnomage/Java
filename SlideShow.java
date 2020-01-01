df189ee96cf86f3feda96e676632ceeaaed931ce
johnomage <31286651+johnomage@users.noreply.github.com>
2020-01-01 22:48:42 +0100

    Update and rename slideShow to SlideShow.java

--- SlideShow.java
+++ SlideShow.java
@@ -1,5 +1,3 @@
-package EventDrivenProgAndAnim;
-
 import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -23,7 +21,7 @@ public class Exercise15_30 extends Application{
 	static String file1, file2, file3, file4;
 	static Label label1, label2, label3, label4;
 	static ImageView view001, view002, view003, view004;
-	static String root = "file:///C:/Users/Honeeks Inc/Pictures/Ultra.HD.Wallpaper.Pack.20/";
+	static String root = ""; //Images root directory here
 	static String[] name = {"001",	"002",	"003",	"004",	"005",	"006",	"007",	"008",	"009",	"010",	"011",	"012",	"013",	"014",	"015",	"016",	"017",	"018",	"019",	"020",	"021",	"022",	"023",	"024",	"025",	"026",	"027",	"028",	"029",	"030",	"031",	"032",	"033",	"034",	"035",	"036",	"037",	"038",	"039",	"040",	"041",	"042",	"043",	"044",	"045",	"046",	"047",	"048",	"049",	"050",	
 			  "051",	"052",	"053",	"054",	"055",	"056",	"057",	"058",	"059",	"060",	"061",	"062",	"063",	"064",	"065",	"066",	"067",	"068",	"069",	"070",	"071",	"072",	"073",	"074",	"075",	"076",	"077",	"078",	"079",	"080",	"081",	"082",	"083",	"084",	"085",	"086",	"087",	"088",	"089",	"090",	"091",	"092",	"093",	"094",	"095",	"096",	"097",	"098",	"099",	"100",};
 
